20b896280de00bec7863f0e21b41c5a27032d1c9
n33d <88423458+gundaIf@users.noreply.github.com>
2026-03-19 16:05:46 +0530

    Update background.js
    Filtered out home and main logo redirection.

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
-// Redirect youtube.com homepage to subscriptions feed
 chrome.webNavigation.onCommitted.addListener((details) => {
   const url = new URL(details.url);
 
-  // Only redirect the main page, not all YouTube pages
   const isHomepage =
     url.hostname === "www.youtube.com" &&
     (url.pathname === "/" || url.pathname === "") &&
